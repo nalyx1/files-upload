b5bb7f3bf15998366fcb54fce7fd92cce4fb2024
João Rodrigues <54764344+nalyx1@users.noreply.github.com>
2023-01-20 15:03:49 -0300

    fix: try catch on controller

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -3,19 +3,24 @@ import { Request, Response } from 'express'
 
 class Controller {
   async handle(request: Request, response: Response) {
-    const { file } = request
+    try {
+      const { file } = request
 
-    if (file) {
-      const post = await prisma.file.create({
-        data: {
-          name: file.originalname,
-          size: file.size,
-          key: file.filename,
-          url: '',
-        },
-      })
+      if (file) {
+        const post = await prisma.file.create({
+          data: {
+            name: file.originalname,
+            size: file.size,
+            key: file.filename,
+            url: '',
+          },
+        })
 
-      response.status(200).send(post)
+        response.status(200).send(post)
+      }
+    } catch (error) {
+      console.log(error)
+      response.status(500).send(error)
     }
   }
 }
